c802a2421069e7b089573706ff9af6988c754052
aps180180 <aps180180@gmail.com>
2024-10-27 17:25:49 -0300

    feat: getbyId

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import express from "express";
-import {connectToDatabase} from "./db/connect.js";
+import { connectToDatabase } from "./db/connect.js";
 import { Book } from "./models/book.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -16,6 +17,33 @@ app.post("/api/v1/books", async (req, res) => {
   } catch (error) {
     console.error("Error saving book: ", error);
     res.status(500).json({ sucess: false, error: "Error saving book" });
+  }
+});
+
+app.get("/api/v1/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json({ success: true, data: books });
+  } catch (error) {
+    console.error("Error fetching books: ", error);
+    res.status(500).json({ success: false, error: "Error fetching books" });
+  }
+});
+
+app.get("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ success: false, error: "Inválid Id" });
+  }
+  try {
+    const book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+    res.status(200).json({ success: true, data: book });
+  } catch (error) {
+    console.error("Error fetching book: ", error);
+    res.status(500).json({ success: false, error: "Error fetching book" });
   }
 });
 
