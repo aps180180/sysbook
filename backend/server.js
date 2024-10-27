5d8c5dd98607788b8006dd3c48095887332e32b7
aps180180 <aps180180@gmail.com>
2024-10-27 14:53:57 -0300

    feat: add database integration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to the Database");
+  } catch (error) {
+    console.error("Error connecting the databaase: ", error);
+    process.exit(1);
+  }
+}
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+  connectToDatabase();
 });
+
+// m4rJhtXzochPwtXE
